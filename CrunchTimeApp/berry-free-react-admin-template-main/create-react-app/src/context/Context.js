72b5890713938a71a2e8295b008365a8197c08f3
ManasaGDL <79443696+ManasaGDL@users.noreply.github.com>
2024-01-29 15:19:11 +0530

    Delete CrunchTimeApp/berry-free-react-admin-template-main/create-react-app directory

--- CrunchTimeApp/berry-free-react-admin-template-main/create-react-app/src/context/Context.js
+++
@@ -1,5 +0,0 @@
-import { createContext} from "react";
-
-
-export const Context = createContext(localStorage.getItem('data'));
-export const weekContext = createContext(localStorage.getItem('week')||'')
